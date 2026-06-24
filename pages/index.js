579eddaa1002ce20bfe71ab460ecfe0f86496683
mostafa <abdelrahman.tiffa1517@gmail.com>
2026-06-24 12:06:21 +0300

    Delete pages/index.js

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: "center", marginTop: "50px", fontFamily: "Arial" }}>
-      <h1>Playfinity</h1>
-      <p>Marketplace for Egypt (Buy & Sell New & Used Items)</p>
-    </div>
-  );
-}
